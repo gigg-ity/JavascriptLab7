f87f41440229742c123b237f277cf4274d277ffc
gigg-ity <ahmad_cheema94@hotmail.com>
2025-04-03 22:33:45 -0400

    Added Form Event

--- script.js
+++ script.js
@@ -19,3 +19,10 @@ const keyDisplay = document.getElementById('keyDisplay');
 keyboardInput.addEventListener('keyup', function(a){
     keyDisplay.textContent = a.key;
 });
+
+const form = document.getElementById('form');
+const formMesssage = document.getElementById('formMesssage');
+form.onsubmit = function(a){
+    a.preventDefault();
+    formMessage.textContent = "Form submitted";
+};
